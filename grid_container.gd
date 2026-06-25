f56bc46be376bae7b2e1b093247eacba658f71b8
Peachgmo <lookpeachshh@gmail.com>
2026-06-25 16:51:48 -0300

    Cria um Jogo da Velha

---
+++ grid_container.gd
@@ -0,0 +1,45 @@
+extends Control
+
+var jogador1 = "X"
+var quadro =[]
+
+@onready var botoes = $GridContainer.get_children()
+
+func _ready():
+	for i in range(9):
+		quadro.append("")
+		botoes[i].pressed.connect(_on_button_pressed.bind(i))
+
+func _on_button_pressed(index):
+	if quadro[index] != "":
+		return
+		
+	quadro[index] = jogador1
+	botoes[index].text = jogador1
+	
+	if check_vencedor():
+		print("Vencedor: ", jogador1)
+		reset_game()
+		return
+		
+	jogador1 = "O" if jogador1 == "X" else "X"
+	
+func check_vencedor():
+	var wins = [
+		[0, 1, 2], [3, 4, 5], [6, 7, 8], 
+		[0, 3, 6], [1, 4, 7], [2, 5, 8],
+		[0, 4, 8], [2, 4, 6]
+	]
+	
+	for combo in wins:
+		if quadro[combo[0]] != "" and quadro[combo[0]] == quadro[combo[1]] and quadro[combo[1]] == quadro[combo[2]]:
+			return true
+			
+	return false
+	
+func reset_game():
+	for i in range(9):
+		quadro[i] = ""
+		botoes[i] == ""
+		
+		jogador1="X"
